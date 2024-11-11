3e228a5591001940f42c4a040b5b8fd9e7717bd1
Jonathan Posada <jonathanposada@Jonathans-MacBook-Pro.local>
2024-11-10 16:29:46 -0800

    Implement recursive solution, add simple tests for even/odd

--- sequence/main.py
+++ sequence/main.py
@@ -2,11 +2,25 @@ def layup_sequence(n: int) -> int:
 	"""
 	Implement the Layup Sequence defined for n as:
 	S(n) =
-	  1 if n = 1,
-	  2 if n = 2,
-	  S(n-1) + S(n-2)   if n is even
-	  2*S(n-1) - S(n-2) if n is odd
-	"""
+		1 if n = 1
+		2 if n = 2
+		S(n-1) + S(n-2)   if n is even
+		2*S(n-1) - S(n-2) if n is odd
 
-	return 0
+	Returns:
+		The value of the layup sequence for n. Returns null if n <= 0 or n is not an integer.
+	"""
+	if type(n) != int:
+		return None
+
+	if (n <= 0):
+		return None
+
+	if (n == 1 or n == 2):
+		return n
+
+	if (n % 2 == 0):
+		return layup_sequence(n-1) + layup_sequence(n-2)
+	else:
+		return 2*layup_sequence(n-1) - layup_sequence(n-2)
 
--- sequence/test.py
+++ sequence/test.py
@@ -3,3 +3,9 @@ from main import layup_sequence
 def test_base_cases():
 	assert layup_sequence(1) == 1
 	assert layup_sequence(2) == 2
+
+def test_simple_odd():
+	assert layup_sequence(3) == 3
+
+def test_simple_even():
+	assert layup_sequence(4) == 5
