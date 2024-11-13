50dadb7b0399261886dc604b969fd806469431fe
Jonathan Posada <jonathanposada@Jonathans-MBP.lan>
2024-11-12 22:52:27 -0800

    Basic functionality done, yaw + speed control working well

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from turtle import Turtle
 
+YAW_CHANGE_SIZE = 10
+
 
 class Airplane:
+
+
 
 	def __init__(self):
 		self.turtle = Turtle()
 		self.screen = self.turtle.screen
+		self.flight_speed = 1
+
 		self.setup_window()
 		self.setup_cursor()
 		self.setup_listeners()
@@ -21,27 +27,34 @@ class Airplane:
 	def setup_listeners(self):
 		self.screen.onkey(self.increase_speed, "Up")
 		self.screen.onkey(self.decrease_speed, "Down")
-		self.screen.onkey(self.yaw_lef, "Left")
+		self.screen.onkey(self.yaw_left, "Left")
 		self.screen.onkey(self.yaw_right, "Right")
 		self.screen.listen()
 
 	def increase_speed(self):
-		print("Current speed: " + str(self.turtle.speed()))
-		self.turtle.speed(self.turtle.speed() + 1)
+		# print("Current speed: " + str(self.turtle.speed()))
+		# self.turtle.speed(self.turtle.speed() + 1)
+		self.flight_speed = self.flight_speed + 1
 
 	def decrease_speed(self):
-		print("Current speed: " + str(self.turtle.speed()))
-		self.turtle.speed(self.turtle.speed() - 1)
+		# print("Current speed: " + str(self.turtle.speed()))
+		# self.turtle.speed(self.turtle.speed() - 1)
+		self.flight_speed = self.flight_speed - 1
+
+	def yaw_left(self):
+		self.turtle.left(YAW_CHANGE_SIZE)
+
+	def yaw_right(self):
+		self.turtle.right(YAW_CHANGE_SIZE)
+
+	def active_fly(self):
+		self.turtle.forward(1 * self.flight_speed)
+		self.screen.ontimer(self.active_fly, 100)
 
 
 	def fly(self):
-		for _ in range(10):
-			self.turtle.forward(100)
-			self.turtle.left(90)
-			self.turtle.forward(50)
-			self.turtle.left(10)
-			self.turtle.forward(50)
-			self.turtle.home()
+		self.screen.ontimer(self.active_fly, 250)
+		self.screen.mainloop()
 
 
 def main():
